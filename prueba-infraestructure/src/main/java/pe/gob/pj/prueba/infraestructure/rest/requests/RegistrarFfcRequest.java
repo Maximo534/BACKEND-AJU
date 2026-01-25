968fe22a8293b96fd3423308748daa256f7b8074
lcaiguaray <luiscaiguaraycc@gmail.com>
2026-01-25 05:48:07 -0500

    Ajuste fortalecimiento

--- prueba-infraestructure/src/main/java/pe/gob/pj/prueba/infraestructure/rest/requests/RegistrarFfcRequest.java
+++ prueba-infraestructure/src/main/java/pe/gob/pj/prueba/infraestructure/rest/requests/RegistrarFfcRequest.java
@@ -126,6 +126,8 @@ public class RegistrarFfcRequest implements Serializable {
     public static class DetalleTareaRequest implements Serializable {
         @NotBlank(message = "El ID de la tarea es obligatorio")
         private String tareaId;
+
+        @NotNull(message = "La fecha de inicio de la tarea es obligatoria")
         private LocalDate fechaInicio;
     }
 }
